fe01bcd7baa41cb2653cee9bd33b6e39ff802a23
Chris <petermanchristoph@gmail.com>
2023-02-03 11:27:05 -0500

    Initial commit

---
+++ src/main/java/com/catabase/CatabaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.catabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatabaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatabaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catabase/CatabaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatabaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

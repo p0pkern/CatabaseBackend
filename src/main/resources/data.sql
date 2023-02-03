09396b33543c2b534486c623c729a068ca1f1aef
Chris <petermanchristoph@gmail.com>
2023-02-03 16:59:49 -0500

    fixed issues with data.sql that improperly stored image url, modified all method in controller, did revert as it didn't work

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO cat (id, name, image) VALUES (1, 'Orange Tabby', '../cats/otabby.jpeg')
-INSERT INTO cat (id, name, image) VALUES (2, 'Black Cat', '../cats/black.jpeg')
+INSERT INTO cat (id, name, image) VALUES (1, 'Orange Tabby', './imgs/otabby.jpeg')
+INSERT INTO cat (id, name, image) VALUES (2, 'Black Cat', './imgs/black.jpg')

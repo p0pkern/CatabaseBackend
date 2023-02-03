dd013a3e67aafbb20f98038d5acf09300b5a6fe0
Chris <petermanchristoph@gmail.com>
2023-02-03 11:32:10 -0500

    Refactored name of app to CatabaseServer

--- src/main/java/com/catabase/CatabaseServer.java
+++ src/main/java/com/catabase/CatabaseServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatabaseBackendApplication {
+public class CatabaseServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatabaseBackendApplication.class, args);
+		SpringApplication.run(CatabaseServer.class, args);
 	}
 
 }

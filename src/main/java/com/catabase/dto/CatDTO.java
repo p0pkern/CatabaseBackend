af1c868a44ca4ebb6d75e2625c136a6858469fa7
Chris <petermanchristoph@gmail.com>
2023-02-06 14:47:37 -0500

    Updated Entity/DTO to match wikipedia data

--- src/main/java/com/catabase/dto/CatDTO.java
+++ src/main/java/com/catabase/dto/CatDTO.java
@@ -1,26 +1,32 @@
 package com.catabase.dto;
 
+import java.util.Objects;
+
 import com.catabase.entity.Cat;
 
 public class CatDTO {
 	private long id;
-	private String name;
-	private String image;
+	private String breed;
+	private String location;
+	private String type;
+	private String bodyType;
+	private String coatType;
+	private String coatPattern;
+	private String imageLocation;
+	
+	public CatDTO() {}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
+	public CatDTO(long id, String breed, String location, String type, String bodyType, String coatType,
+			String coatPattern, String imageLocation) {
+		super();
+		this.id = id;
+		this.breed = breed;
+		this.location = location;
+		this.type = type;
+		this.bodyType = bodyType;
+		this.coatType = coatType;
+		this.coatPattern = coatPattern;
+		this.imageLocation = imageLocation;
 	}
 
 	public long getId() {
@@ -30,12 +36,98 @@ public class CatDTO {
 	public void setId(long id) {
 		this.id = id;
 	}
-	
-	public static Cat prepareCatEntity(CatDTO catDTO) {
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBodyType() {
+		return bodyType;
+	}
+
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+
+	public String getCoatType() {
+		return coatType;
+	}
+
+	public void setCoatType(String coatType) {
+		this.coatType = coatType;
+	}
+
+	public String getCoatPattern() {
+		return coatPattern;
+	}
+
+	public void setCoatPattern(String coatPattern) {
+		this.coatPattern = coatPattern;
+	}
+
+	public String getImageLocation() {
+		return imageLocation;
+	}
+
+	public void setImageLocation(String imageLocation) {
+		this.imageLocation = imageLocation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(breed, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CatDTO other = (CatDTO) obj;
+		return Objects.equals(breed, other.breed) && id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [id=" + id + ", breed=" + breed + ", location=" + location + ", type=" + type + ", bodyType="
+				+ bodyType + ", coatType=" + coatType + ", coatPattern=" + coatPattern + ", imageLocation="
+				+ imageLocation + "]";
+	}
+
+	public static Cat getCatEntity(CatDTO catDTO) {
 		Cat cat = new Cat();
 		cat.setId(catDTO.getId());
-		cat.setImage(cat.getImage());
-		cat.setName(catDTO.getName());
+		cat.setBreed(catDTO.getBreed());
+		cat.setBodyType(catDTO.getBodyType());
+		cat.setCoatPattern(catDTO.getCoatPattern());
+		cat.setCoatType(catDTO.getCoatType());
+		cat.setLocation(catDTO.getLocation());
+		cat.setType(catDTO.getLocation());
+		cat.setImageLocation(catDTO.getImageLocation());
+		
 		return cat;
 	}
 
--- src/main/java/com/catabase/entity/Cat.java
+++ src/main/java/com/catabase/entity/Cat.java
@@ -1,5 +1,9 @@
 package com.catabase.entity;
 
+import java.util.Objects;
+
+import com.catabase.dto.CatDTO;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,25 +25,27 @@ public class Cat {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SequenceSubCategoryId")
 	@SequenceGenerator(name = "SequenceSubCategoryId", sequenceName = "SUB_CATEGORY_SEQ")
 	private long id;
-	private String name;
-	private String image;
+	private String breed;
+	private String location;
+	private String type;
+	private String bodyType;
+	private String coatType;
+	private String coatPattern;
+	private String imageLocation;
 	
 	public Cat() {}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
+	public Cat(long id, String breed, String location, String type, String bodyType, String coatType,
+			String coatPattern, String imageLocation) {
+		super();
+		this.id = id;
+		this.breed = breed;
+		this.location = location;
+		this.type = type;
+		this.bodyType = bodyType;
+		this.coatType = coatType;
+		this.coatPattern = coatPattern;
+		this.imageLocation = imageLocation;
 	}
 
 	public long getId() {
@@ -49,10 +55,99 @@ public class Cat {
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBodyType() {
+		return bodyType;
+	}
+
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+
+	public String getCoatType() {
+		return coatType;
+	}
+
+	public void setCoatType(String coatType) {
+		this.coatType = coatType;
+	}
+
+	public String getCoatPattern() {
+		return coatPattern;
+	}
+
+	public void setCoatPattern(String coatPattern) {
+		this.coatPattern = coatPattern;
+	}
+
+	public String getImageLocation() {
+		return imageLocation;
+	}
+
+	public void setImageLocation(String imageLocation) {
+		this.imageLocation = imageLocation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(breed, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cat other = (Cat) obj;
+		return Objects.equals(breed, other.breed) && id == other.id;
+	}
+
 	@Override
 	public String toString() {
-		return "Id = " + id + " Name = " + name + " imageSrc = " + image;
+		return "Cat [id=" + id + ", breed=" + breed + ", location=" + location + ", type=" + type + ", bodyType="
+				+ bodyType + ", coatType=" + coatType + ", coatPattern=" + coatPattern + ", imageLocation="
+				+ imageLocation + "]";
+	}
+
+	public static CatDTO getCatDTO(Cat cat) {
+		CatDTO catDTO = new CatDTO();
+		catDTO.setId(cat.getId());
+		catDTO.setBreed(cat.getBreed());
+		catDTO.setBodyType(cat.getBodyType());
+		catDTO.setCoatPattern(cat.getCoatPattern());
+		catDTO.setCoatType(cat.getCoatType());
+		catDTO.setLocation(cat.getLocation());
+		catDTO.setType(cat.getLocation());
+		catDTO.setImageLocation(cat.getImageLocation());
+		
+		return catDTO;
 	}
 	
 }
